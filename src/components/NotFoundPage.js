a1060032d7fad7d3dc2d7f5f2f97dbe71803ccca
Nicholas Nguyen <nnguyen@barracuda.com>
2020-02-25 16:40:06 -0800

    Added router

---
+++ src/components/NotFoundPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const NotFoundPage = () => (
+    <div>
+        Rut Ro Woof 404 Woof - <Link to="/">Go Home</Link>
+    </div>
+);
+
+export default NotFoundPage;
