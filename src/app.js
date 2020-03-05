df4f72661efefc46e5c1ac79ae462637a1ba77ae
Nicholas Nguyen <nnguyen@barracuda.com>
2020-03-04 22:37:17 -0800

    Added React with Redux

--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
+import AppRouter from './routers/AppRouter'
+import configureStore from './store/configureStore'
+import getVisiableExpenses from './selectors/expenses'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { addExpense } from './actions/expenses'
+import { setTextFilter } from './actions/filters'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
-import AppRouter from './routers/AppRouter'
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'))
+const store = configureStore()
+
+store.dispatch(addExpense({ description: 'Water Bill', amount: 4500 }))
+store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 1000 }))
+store.dispatch(addExpense({ description: 'Rent', amount: 109500 }))
+
+const state = store.getState()
+const visibleExpenses = getVisiableExpenses(state.expenses, state.filters)
+console.log(visibleExpenses)
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+)
+
+ReactDOM.render(jsx, document.getElementById('app'))
---
+++ src/reducers/expenses.js
@@ -0,0 +1,23 @@
+// Expenses reducer
+const expensesReducerState = []
+
+export default (state = expensesReducerState, action) => {
+    switch (action.type) {
+        case 'ADD_EXPENSE':
+            return [...state, action.expense]
+        case 'EDIT_EXPENSE':
+            return state.map((expense) => {
+                if (expense.id === action.id) {
+                    return {
+                        ...expense, ...action.updates
+                    } 
+                } else {
+                   return expense
+                }
+             })
+        case 'REMOVE_EXPENSE':
+            return state.filter(({id}) => id !== action.id)
+        default:
+           return state 
+    }
+}
